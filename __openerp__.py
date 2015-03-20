621adbd0d8dc58d4dab0cde30ffaa6774077b400
krocket <fabian.monoyer@gmail.com>
2015-03-20 22:41:54 +0100

    nouveau fichier: __init__.py 	nouveau fichier: __init__.pyc 	nouveau fichier: __openerp__.py 	nouveau fichier: static/description/icon.png 	nouveau fichier: static/description/img1.png 	nouveau fichier: static/description/index.html 	nouveau fichier: static/src/css/font-awesome.min.css 	nouveau fichier: static/src/fonts/FontAwesome.otf 	nouveau fichier: static/src/fonts/fontawesome-webfont.eot 	nouveau fichier: static/src/fonts/fontawesome-webfont.svg 	nouveau fichier: static/src/fonts/fontawesome-webfont.ttf 	nouveau fichier: static/src/fonts/fontawesome-webfont.woff 	nouveau fichier: static/src/fonts/fontawesome-webfont.woff2 	nouveau fichier: static/src/js/website.editor.js 	nouveau fichier: views/themes.xml

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Font Awesome 4.3.0",
+    'category': 'Website',
+    'summary': 'Add fonts awesome 4.3.0',
+    'version': '1.0',
+    'description': """
+
+    Font Awesome, new icons in version 4.3.
+
+        """,
+    'author': "O'Labs",
+    'depends': ['website'],
+    'data': [
+        'views/themes.xml',
+        ],
+     'installable': True,
+     'application': False,
+}
